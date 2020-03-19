0cfb16ad32c5e3c74245809fceaa88bcfc6a909a
Malka Bachrach <malka.bachrach@ti.htc.edu>
2020-03-18 21:25:37 -0500

    first commit

---
+++ src/test/java/walder/benchmark/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package walder.benchmark.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
